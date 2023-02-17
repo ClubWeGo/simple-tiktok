7bae75e7a13c4008708b4baba355d90475edf436
Darley <darley.wey@gmail.com>
2023-02-17 21:01:59 +0800

    favorite.thrift结构体变更

--- idl/favoritemicro.thrift
+++ idl/favoritemicro.thrift
@@ -1,24 +1,5 @@
 namespace go favorite
 
-struct User {
-    1: required i64 id;
-    2: required string name;
-    3: optional i64 follow_count;
-    4: optional i64 follower_count;
-    5: required bool is_follow;
-}
-
-struct Video {
-    1: required i64 id;
-    2: required User author;
-    3: required string play_url;
-    4: required string cover_url;
-    5: required i64 favorite_count;
-    6: required i64 comment_count;
-    7: required bool is_favorite;
-    8: required string title;
-}
-
 
 # favoriteList
 struct FavoriteListReq {
@@ -28,7 +9,7 @@ struct FavoriteListReq {
 struct FavoriteListResp {
     1: required i32 status_code;
     2: optional string status_msg;
-    3: required list<Video> video_list;
+    3: required list<i64> video_id_list;
 }
 
 
