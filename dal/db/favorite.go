f9e7bc4f2f3ccd08bb6b606d5c063f4222a99531
Darley <darley.wey@gmail.com>
2023-02-17 17:04:56 +0800

    实现favorite所需数据库方法

--- dal/db/favorite.go
+++ dal/db/favorite.go
@@ -2,66 +2,65 @@ package db
 
 import (
 	"context"
+	"github.com/ClubWeGo/favoritemicro/dal/model"
 	. "github.com/ClubWeGo/favoritemicro/dal/query"
-	"log"
 )
 
-//var Q = query.Q
-
 // GetFavoriteRelation get favorite video info
-/*func GetFavoriteRelation(ctx context.Context, uid int64, vid int64) ([]*model.Video, error) {
-	//user := new(model.User)
-	user, err := userQ.WithContext(ctx).Where(userQ.ID.Eq(uint(uid))).First()
+func GetFavoriteRelation(ctx context.Context, uid uint, vid uint) error {
+	_, err := Favorite.WithContext(ctx).Where(Favorite.UserId.Eq(uid), Favorite.VideoId.Eq(vid)).First()
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	//video := new(Video)
-	videos, err := favoriteQ.WithContext(ctx).Where(favoriteQ.UserId.Eq(int(uid))).Find()
-	if err != nil {
-		return nil, err
-	}
-
-	//if err := DB.WithContext(ctx).Model(&user).Association("FavoriteVideos").Find(&video, vid); err != nil {
-	//	return nil, err
-	//}
-	return videos, nil
-}*/
+	return nil
+}
 
 func AddFavorite(ctx context.Context, uid uint, vid uint) error {
-	log.Println("add favorite")
+	/*log.Println("add favorite")
 	user, err := User.WithContext(ctx).Where(User.ID.Eq(uid)).First()
 	if err != nil {
 		return err
 	}
 	log.Println(user.ID)
-	return nil
+	return nil*/
 
-	//return Q.Transaction(func(tx *query.Query) error {
-	//	// 在事务中执行一些 db 操作（从这里开始，您应该使用 'tx' 而不是 'db'）
-	//	//1. 新增点赞数据
-	//	//user, err := tx.User.WithContext(ctx).Where(tx.User.ID.Eq(uid)).First()
-	//	//if err != nil {
-	//	//	return err
-	//	//}
-	//	//
-	//	//video, err := tx.Video.WithContext(ctx).Where(tx.Video.ID.Eq(vid)).First()
-	//	//if err != nil {
-	//	//	return err
-	//	//}
-	//
-	//	if err := tx.Favorite.WithContext(ctx).Create(&Favorite{UserId: uid, VideoId: vid}); err != nil {
-	//		return err
-	//	}
-	//	//2.改变 video 表中的 favorite count
-	//	res, err := tx.Video.WithContext(ctx).Where(tx.Video.ID.Eq(vid)).UpdateSimple(tx.Video.Comment_count.Add(1))
-	//	if err != nil {
-	//		return err
-	//	}
-	//
-	//	if res.RowsAffected != 1 {
-	//		return err
-	//	}
-	//	return nil
-	//})
+	return Q.Transaction(func(tx *Query) error {
+		// 在事务中执行一些 db 操作（从这里开始，您应该使用 'tx' 而不是 'db'）
+		//1. 新增点赞数据
+		if err := tx.Favorite.WithContext(ctx).Create(&model.Favorite{UserId: uid, VideoId: vid}); err != nil {
+			return err
+		}
+		//2.改变 video 表中的 favorite count
+		_, err := tx.Video.WithContext(ctx).Where(tx.Video.ID.Eq(vid)).UpdateSimple(tx.Video.Comment_count.Add(1))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func DeleteFavorite(ctx context.Context, uid uint, vid uint) error {
+	return Q.Transaction(func(tx *Query) error {
+		// 在事务中执行一些 db 操作（从这里开始，您应该使用 'tx' 而不是 'db'）
+		//1. 删除点赞数据
+		if _, err := tx.Favorite.WithContext(ctx).Where(tx.Favorite.UserId.Eq(uid), tx.Favorite.VideoId.Eq(vid)).Delete(); err != nil {
+			return err
+		}
+		//2.改变 video 表中的 favorite count
+		_, err := tx.Video.WithContext(ctx).Where(tx.Video.ID.Eq(vid)).UpdateSimple(tx.Video.Comment_count.Sub(1))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// GetFavoriteList get favorite video list
+func GetFavoriteList(ctx context.Context, uid uint) ([]*model.Video, error) {
+	vidsQuery := Favorite.WithContext(ctx).Select(Favorite.VideoId).Where(Favorite.UserId.Eq(uid))
+	videos, err := Video.WithContext(ctx).Where(Favorite.WithContext(ctx).Columns(Video.ID).In(vidsQuery)).Find()
+	if err != nil {
+		return nil, err
+	}
+	return videos, nil
 }
