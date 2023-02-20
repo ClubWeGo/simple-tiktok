b1f69d7b11a96309bca44d1549e0bac52ec62e12
zxhchime <ubuntu@localhost.localdomain>
2023-02-20 13:38:40 +0800

    fix:idl 数据结构变更

--- idl/relation.thrift
+++ idl/relation.thrift
@@ -1,11 +1,17 @@
 namespace go relation
 
 struct User {
-    1: required i64 id;
-    2: required string name;
-    3: optional i64 follow_count;
-    4: optional i64 follower_count;
-    5: required bool is_follow;
+    1:  required i64        id;
+    2:  required string     name;               // 用户昵称
+    3:  optional i64        follow_count;       // 关注数
+    4:  optional i64        follower_count;     // 粉丝数
+    5:  required bool       is_follow;          // 是否关注 true-已关注 false-未关注
+    6:  optional string     avatar;             // 头像
+    7:  optional string     background_image;   // 个人顶部大图
+    8:  optional string     signature;          // 个人简介
+    9:  optional i64        total_favorited;    // 获赞数
+    10: optional i64        work_count;         // 作品数
+    11: optional i64        favorite_count;     // 喜欢数
 }
 
 
